ProjectEverest-staging/frameworks_base

SystemUI: Show a notification toast when reloading system theme
* Some users may mistakenly think that the loading is a lag or a crash. Show a toast notification so users will wait until the toast disappears.

Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -460,7 +460,7 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
         mKeyguardTransitionInteractor = keyguardTransitionInteractor;
         mUiModeManager = uiModeManager;
         dumpManager.registerDumpable(TAG, this);
-        mThemeController = new EverestThemeController(mContext.getContentResolver(), mBgHandler);
+        mThemeController = new EverestThemeController(mContext, mBgHandler);
     }
 
     @Override

tavukkdoner/android_frameworks_base_rising

packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -477,7 +477,7 @@ public class ThemeOverlayController implements CoreStartable, Dumpable, TunerSer
         mUiModeManager = uiModeManager;
         mTunerService = tunerService;
         dumpManager.registerDumpable(TAG, this);
-        mThemeController = new RisingThemeController(mContext.getContentResolver(), mBgHandler);
+        mThemeController = new RisingThemeController(mContext, mBgHandler);
     }
 
     @Override